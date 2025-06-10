225940ae48d4a5ccc33f729f78d9f52cdce2b536
dudesahn <dudesahn@pm.me>
2025-06-10 16:14:49 -0400

    feat: review
    * remove duplicate harvestTrigger override
    * update natspec

--- contracts/ShareValueHelper.sol
+++ contracts/ShareValueHelper.sol
@@ -14,6 +14,7 @@ library ShareValueHelper {
      * @notice Helper function to convert underlying amount to vault shares with exact precision.
      * @param _vault The address of the vault token.
      * @param _amount The amount of underlying to convert to shares.
+     * @param _useCeiling Whether to round up or not.
      * @return The shares of vault token.
      */
     function amountToShares(
@@ -39,6 +40,7 @@ library ShareValueHelper {
      * @notice Helper function to convert shares to underlying amount with exact precision.
      * @param _vault The address of the vault token.
      * @param _shares The amount of shares to convert to underlying.
+     * @param _useCeiling Whether to round up or not.
      * @return The amount of underlying token.
      */
     function sharesToAmount(
